2c2164f7b055dd22a97c1a3d74281ec1c2aa15f7
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-07-13 06:12:07 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main -program that prints the size of various types on the compute * r it is compiled and run on.
+ * main -program that prints the size of various types
+ * on the compute * r it is compiled and run on.
  * Return: 0 successful execution
 */
 int main(void)
@@ -9,11 +10,11 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float f
-puts("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(a));
-puts("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(b));
-puts("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(c));
-puts("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(d));
-puts("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(f));
+	float f;
+puts("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
+puts("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(b));
+puts("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(c));
+puts("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(d));
+puts("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
