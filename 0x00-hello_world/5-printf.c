b38fbf21f04b37c23ef971a6d3f10b699cdb355d
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-07-13 05:55:46 -0700

    Hello, puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -program that prints a linefollowed by a new line:
+ * Use the function printf
+ * You are not allowed to use puts
+ * Return: 0 successful execution
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
