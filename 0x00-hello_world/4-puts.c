2b76941b67a1c2958987c924bcc14d3f87c57b69
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-07-13 05:44:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/**
+/*
  * main -program that prints a linefollowed by a new line:
  * Use the function puts
  * You are not allowed to use printf
- * return - the value 0
+ * return -the value 0
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
