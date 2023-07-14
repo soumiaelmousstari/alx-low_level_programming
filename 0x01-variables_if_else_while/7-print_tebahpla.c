0741baea4a0fa0117babd0e2e6fe7691a4839c78
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-07-14 15:35:23 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Write a program that prints the lowercase
+ * alphabet in reverse, followed by a new line.
+ * Return: 0 saccucc instructions
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+return (0);
+}
