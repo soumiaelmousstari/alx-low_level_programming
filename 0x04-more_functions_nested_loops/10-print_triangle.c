eb213da00335c628feba732cfb73213512ce175d
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-07-21 08:47:13 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	{
 		for (x = 0; x < size; x++)
 		{
-			for (y = size - x; y > 1; x--)
+			for (y = size - x; y > 1; y--)
 				_putchar(32);
 			for (z = 0; z <= x; z++)
 				_putchar(35);
