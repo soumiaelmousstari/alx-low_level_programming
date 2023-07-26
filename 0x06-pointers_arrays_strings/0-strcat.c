77438aebf2e23d0dc9abe8287cea499aa958286a
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-07-26 10:33:22 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat -a function that concatenates two strings.
+ * @dest: the string to be appended upon
+ * @src: the string to be appended to dest
+ * Return: a pointer to the resulting string des
+*/
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0, n;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
