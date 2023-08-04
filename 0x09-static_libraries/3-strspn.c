b945381f58b3b4b463bd168d339335b36493acca
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-08-04 16:45:47 -0700

    1. Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -9,20 +9,22 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int bytes = 0;
+	int index;
 
-	value = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		check = 0;
-		for (n = 0; accept[n] != '\0'; n++)
+		for (index = 0; accept[index]; index++)
 		{
-			if (accept[n] == s[i])
+			if (*s == accept[index])
 			{
-				value++;
-				check = 1;
+				bytes++;
+				break;
 			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
 		}
+		s++;
 	}
-	return (0);
+	return (bytes);
 }
