df3f4d944f235ebbdacc209cf0630b4c1fc84511
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-08-24 16:22:50 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_before_main -a function that prints
+ * You're beat! and yet, you must allow,\nI
+ * bore my house upon my back!\n before the main
+ * function is executed.
+ * Return: ...
+*/
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
+/**
+ * main -the main fonction
+ * Return: always 0
+*/
+int main(void)
+{
+	return (0);
+}
