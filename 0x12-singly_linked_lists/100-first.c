4bce06f3ba74cfb9cf91ac61851a35ea20815540
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-08-24 16:24:54 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,7 @@
  * function is executed.
  * Return: ...
 */
+void print_before_main(void) __attribute__((constructor));
 void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n
