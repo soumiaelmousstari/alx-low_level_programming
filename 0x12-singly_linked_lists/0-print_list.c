b31c398ccab793509cfa9a85019f5a4d1772ea43
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-08-24 14:24:29 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_list -a function that prints all
+ * the elements of a list_t list.
+ * @h: head of the list
+ * Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	int index;
+
+	if (h == NULL)
+		return (0);
+	for (index = 1; h->next != NULL; index++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (index);
+}
