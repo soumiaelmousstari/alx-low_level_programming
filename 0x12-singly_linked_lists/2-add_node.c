84c90a9e5fe74b0cdc8ad12c979f8c1002ffb8ca
SOUMIA EL MOUSSTARI <elmousstarisoumia@gmail.com>
2023-08-24 15:27:51 -0700

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,8 +13,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node = malloc(sizeof(list_t));
 
+	if (node == NULL)
+		return (NULL);
 	node->str = strdup(str);
-
 	if (node->str == NULL)
 	{
 		free(node);
